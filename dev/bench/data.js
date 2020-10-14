d5fd4f45358e90368f246fb325c313fa61911f9c
github-action-benchmark <github@users.noreply.github.com>
2020-10-14 14:53:02 -0700

    add Benchmark (pytest) benchmark result for 31ca5c37a544d7a82aa95de6a30f8ba274dd42ed

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1602712382742,
+  "repoUrl": "https://github.com/Sonicadvance1/FEX",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "Sonicadvance1@gmail.com",
+            "name": "Ryan Houdek",
+            "username": "Sonicadvance1"
+          },
+          "committer": {
+            "email": "Sonicadvance1@gmail.com",
+            "name": "Ryan Houdek",
+            "username": "Sonicadvance1"
+          },
+          "distinct": true,
+          "id": "31ca5c37a544d7a82aa95de6a30f8ba274dd42ed",
+          "message": "more argument changes",
+          "timestamp": "2020-10-14T14:51:07-07:00",
+          "tree_id": "923bd49d31d3206bf4115658e4d08d6e862cb7a8",
+          "url": "https://github.com/Sonicadvance1/FEX/commit/31ca5c37a544d7a82aa95de6a30f8ba274dd42ed"
+        },
+        "date": 1602712382193,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "FEX/Scripts/Execute_FEX.py::test_local_execute",
+            "value": 0.08088106212438732,
+            "unit": "iter/sec",
+            "range": "stddev: 0.10789551657649689",
+            "extra": "mean: 12.363833680399694 sec\nrounds: 5"
+          }
+        ]
+      }
+    ]
+  }
+}
