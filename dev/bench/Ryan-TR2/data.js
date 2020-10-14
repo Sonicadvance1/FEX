22601c421844c9fe3290af9fe67937a57588dbcc
github-action-benchmark <github@users.noreply.github.com>
2020-10-14 15:45:29 -0700

    add Benchmark (pytest) benchmark result for 7f504f05d1ca8b4c4c8a214e2d22714dd5032da1

--- dev/bench/Ryan-TR2/data.js
+++ dev/bench/Ryan-TR2/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1602715251595,
+  "lastUpdate": 1602715529496,
   "repoUrl": "https://github.com/Sonicadvance1/FEX",
   "entries": {
     "Benchmark": [
@@ -31,6 +31,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.10909322235437714",
             "extra": "mean: 12.607235049999872 sec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "Sonicadvance1@gmail.com",
+            "name": "Ryan Houdek",
+            "username": "Sonicadvance1"
+          },
+          "committer": {
+            "email": "Sonicadvance1@gmail.com",
+            "name": "Ryan Houdek",
+            "username": "Sonicadvance1"
+          },
+          "distinct": true,
+          "id": "7f504f05d1ca8b4c4c8a214e2d22714dd5032da1",
+          "message": "More changes",
+          "timestamp": "2020-10-14T15:43:28-07:00",
+          "tree_id": "d362b70d84585ccf1f8e06942083284e16d67b1a",
+          "url": "https://github.com/Sonicadvance1/FEX/commit/7f504f05d1ca8b4c4c8a214e2d22714dd5032da1"
+        },
+        "date": 1602715528906,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "FEX/Scripts/Execute_FEX.py::test_native_povray_bench",
+            "value": 0.7989485128099806,
+            "unit": "iter/sec",
+            "range": "stddev: 0.03242322338774505",
+            "extra": "mean: 1.2516451110008346 sec\nrounds: 5"
+          },
+          {
+            "name": "FEX/Scripts/Execute_FEX.py::test_povray_bench",
+            "value": 0.07970667260788612,
+            "unit": "iter/sec",
+            "range": "stddev: 0.17534239873040666",
+            "extra": "mean: 12.546001072199578 sec\nrounds: 5"
           }
         ]
       }
