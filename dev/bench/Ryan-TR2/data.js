1c926fec8ffe612c83c147bc7c1d47badb76258a
github-action-benchmark <github@users.noreply.github.com>
2020-10-14 15:40:51 -0700

    add Benchmark (pytest) benchmark result for bc21c74bfa1c018694648d202fe2ab0382e9f8ff

---
+++ dev/bench/Ryan-TR2/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1602715251595,
+  "repoUrl": "https://github.com/Sonicadvance1/FEX",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "Sonicadvance1@gmail.com",
+            "name": "Ryan Houdek",
+            "username": "Sonicadvance1"
+          },
+          "committer": {
+            "email": "Sonicadvance1@gmail.com",
+            "name": "Ryan Houdek",
+            "username": "Sonicadvance1"
+          },
+          "distinct": true,
+          "id": "bc21c74bfa1c018694648d202fe2ab0382e9f8ff",
+          "message": "Split up bench results by node",
+          "timestamp": "2020-10-14T15:38:49-07:00",
+          "tree_id": "6edcd5f8c21acfa555b6e7a1e4871c3cbb560c35",
+          "url": "https://github.com/Sonicadvance1/FEX/commit/bc21c74bfa1c018694648d202fe2ab0382e9f8ff"
+        },
+        "date": 1602715251060,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "FEX/Scripts/Execute_FEX.py::test_povray_bench",
+            "value": 0.0793195332707,
+            "unit": "iter/sec",
+            "range": "stddev: 0.10909322235437714",
+            "extra": "mean: 12.607235049999872 sec\nrounds: 5"
+          }
+        ]
+      }
+    ]
+  }
+}
