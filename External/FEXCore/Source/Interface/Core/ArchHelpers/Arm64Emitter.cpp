d6799cc4e29b1b1263e8559e102f0d707feb9166
Ryan Houdek <Sonicadvance1@gmail.com>
2022-09-29 20:50:19 -0700

    Ensure Arm64Emitter uses FEX allocator

--- External/FEXCore/Source/Interface/Core/ArchHelpers/Arm64Emitter.cpp
+++ External/FEXCore/Source/Interface/Core/ArchHelpers/Arm64Emitter.cpp
@@ -20,7 +20,9 @@ namespace FEXCore::CPU {
 
 // We want vixl to not allocate a default buffer. Jit and dispatcher will manually create one.
 Arm64Emitter::Arm64Emitter(FEXCore::Context::Context *ctx, size_t size)
-  : vixl::aarch64::Assembler(size, vixl::aarch64::PositionDependentCode)
+  : vixl::aarch64::Assembler((byte*)FEXCore::Allocator::mmap(nullptr, size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0),
+                             size,
+                             vixl::aarch64::PositionDependentCode)
   , EmitterCTX {ctx} {
   CPU.SetUp();
 
