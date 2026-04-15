381af80f4516d341d6cf3c7554b7ccac5547f65d
Ryan Houdek <Sonicadvance1@gmail.com>
2026-04-15 16:29:25 -0700

    FEXCore: Support rpmalloc hooking on win32
    rpmalloc has been leaking in to the low address space on wow64 and
    arm64ec. In order to avoid that we need to setup the same hooking as the
    Linux side and let the our allocations happen with the TOP_DOWN flag
    set.
    
    XXX: For some reason rpmalloc trampoline handler doesn't get overwritten?

--- FEXCore/Source/Utils/AllocatorHooks.cpp
+++ FEXCore/Source/Utils/AllocatorHooks.cpp
@@ -2,6 +2,8 @@
 #include <FEXCore/Utils/PrctlUtils.h>
 #include <FEXCore/Utils/AllocatorDefines.h>
 
+#include "Utils/Allocator.h"
+
 #ifndef _WIN32
 #include <linux/prctl.h>
 #include <sys/mman.h>
@@ -52,6 +54,7 @@ VirtualTHPPtr VirtualTHPControl {VirtualTHPNOP};
 void SetupHooks(size_t PageSize, HookPtrs Ptrs) {
   VirtualName = Ptrs.VirtualName;
   VirtualTHPControl = Ptrs.VirtualTHPControl;
+  InitializeAllocator(PageSize);
 }
 #endif
 
@@ -115,42 +118,39 @@ void InitializeThread() {
   rpmalloc_thread_initialize();
 }
 
-#ifndef _WIN32
+#define pointer_offset(ptr, ofs) (void*)((char*)(ptr) + (ptrdiff_t)(ofs))
 [[nodiscard]]
 constexpr uint64_t AlignUp(uint64_t value, uint64_t size) {
   return value + (size - value % size) % size;
 }
 
 static void* FEX_rp_mmap(size_t size, size_t alignment, size_t* offset, size_t* mapped_size) {
-#define pointer_offset(ptr, ofs) (void*)((char*)(ptr) + (ptrdiff_t)(ofs))
   // If the alignment is less than the operating page size then alignment is guaranteed. Just remove it.
   if (alignment < global_config.page_size) {
     alignment = 0;
   }
 
   size_t map_size = AlignUp(size + alignment, global_config.page_size);
-  auto ptr = fex_mmap_hook(0, map_size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+  void* ptr {};
+#ifndef _WIN32
+  ptr = fex_mmap_hook(0, map_size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
 
   if (ptr == MAP_FAILED) {
     ptr = nullptr;
-  } else {
-#ifndef PR_SET_VMA
-#define PR_SET_VMA 0x53564d41
-#endif
-
-#ifndef PR_SET_VMA_ANON_NAME
-#define PR_SET_VMA_ANON_NAME 0
-#endif
-    prctl(PR_SET_VMA, PR_SET_VMA_ANON_NAME, ptr, map_size, global_config.page_name);
-
-    // Disable HUGEPAGE on allocation from rpmalloc.
-    madvise(ptr, map_size, MADV_NOHUGEPAGE);
   }
+#else
+  ptr = VirtualAlloc(0, map_size, MEM_RESERVE | MEM_COMMIT | MEM_TOP_DOWN, PAGE_READWRITE);
+#endif
 
   if (ptr == nullptr) {
     fprintf(stderr, "Failed to map VMA region.");
     return nullptr;
   }
+
+  VirtualName(global_config.page_name, ptr, map_size);
+
+  // Disable HUGEPAGE on allocation from rpmalloc.
+  VirtualTHPControl(ptr, map_size, FEXCore::Allocator::THPControl::Disable);
 
   if (alignment) {
     size_t padding = ((uintptr_t)ptr & (uintptr_t)(alignment - 1));
@@ -164,8 +164,34 @@ static void* FEX_rp_mmap(size_t size, size_t alignment, size_t* offset, size_t* 
   return ptr;
 }
 
+static void FEX_rp_memory_unmap(void* address, size_t offset, size_t mapped_size) {
+  address = pointer_offset(address, -(int32_t)offset);
+
+#ifndef _WIN32
+  if (fex_munmap_hook(address, mapped_size) == -1) {
+    fprintf(stderr, "Failed to unmap VMA region.");
+  }
+#else
+  if (!VirtualFree(address, 0, MEM_RELEASE)) {
+    fprintf(stderr, "Failed to unmap VMA region.");
+  }
+#endif
+#undef pointer_offset
+}
+
 static void FEX_rp_memory_commit(void* address, size_t size) {
-  // NOP-implementation.
+  if (global_config.disable_decommit) {
+    return;
+  }
+#ifndef _WIN32
+  if (madvise(address, size, MADV_POPULATE_READ)) {
+    fprintf(stderr, "Failed to commit VMA region.");
+  }
+#else
+  if (!VirtualAlloc(address, size, MEM_COMMIT, PAGE_READWRITE)) {
+    fprintf(stderr, "Failed to commit VMA region.");
+  }
+#endif
 }
 
 static void FEX_rp_memory_decommit(void* address, size_t size) {
@@ -173,24 +199,23 @@ static void FEX_rp_memory_decommit(void* address, size_t size) {
     return;
   }
 
+#ifndef _WIN32
   if (madvise(address, size, MADV_DONTNEED)) {
     fprintf(stderr, "Failed to decommit VMA region.");
   }
-}
-
-static void FEX_rp_memory_unmap(void* address, size_t offset, size_t mapped_size) {
-  address = pointer_offset(address, -(int32_t)offset);
-  int Result = fex_munmap_hook(address, mapped_size);
-  if (Result == -1) {
-    fprintf(stderr, "Failed to unmap VMA region.");
+#else
+  if (!VirtualFree(address, size, MEM_DECOMMIT)) {
+    fprintf(stderr, "Failed to decommit VMA region.");
   }
-#undef pointer_offset
+#endif
 }
 
+#ifndef _WIN32
 void SetupAllocatorHooks(mmap_hook_type MMapHook, munmap_hook_type MunmapHook) {
   fex_mmap_hook = MMapHook;
   fex_munmap_hook = MunmapHook;
 }
+#endif
 
 static rpmalloc_interface_t global_interface {
   .memory_map = FEX_rp_mmap,
@@ -207,7 +232,6 @@ void InitializeAllocator(size_t PageSize) {
   rp_mmap_hook = FEX_rp_mmap;
   rp_munmap_hook = FEX_rp_memory_unmap;
 }
-#endif
 
 #elif defined(_WIN32)
 #error "Tried building _WIN32 without jemalloc"
